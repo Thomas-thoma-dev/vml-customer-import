2d7aa38dd18f6434ef01f09e5cbc2a403d9fd201
Thomas-thoma-dev <thoma241996@gmail.com>
2024-07-29 20:49:15 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  @Module         : Customer Import
+ *  @Package        : Vml_CustomerImport
+ *  @Description    : Customer Import
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vml_CustomerImport',
+    __DIR__
+);
